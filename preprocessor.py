b984ab98c9b589e85e97234c64ab375dbaf5ba20
Gunnar <s183737@dtu.dk>
2024-03-18 12:11:52 +0100

    channels and motion artifact correction part 1

--- preprocessor.py
+++ preprocessor.py
@@ -43,3 +43,62 @@ raw3.annotations.delete(np.nonzero(raw3.annotations.description == "15.0"))
 raw4.annotations.delete(np.nonzero(raw4.annotations.description == "15.0"))
 
 # %% 
+picks0 = mne.pick_types(raw0.info, meg=False, eeg=False, fnirs=True)
+picks1 = mne.pick_types(raw1.info, meg=False, eeg=False, fnirs=True)
+picks2 = mne.pick_types(raw2.info, meg=False, eeg=False, fnirs=True)
+picks3 = mne.pick_types(raw3.info, meg=False, eeg=False, fnirs=True)
+picks4 = mne.pick_types(raw4.info, meg=False, eeg=False, fnirs=True)
+
+# %%
+
+dists0 = mne.preprocessing.nirs.source_detector_distances(raw0.info,picks=picks0)
+dists1 = mne.preprocessing.nirs.source_detector_distances(raw1.info,picks=picks1)
+dists2 = mne.preprocessing.nirs.source_detector_distances(raw2.info,picks=picks2)
+dists3 = mne.preprocessing.nirs.source_detector_distances(raw3.info,picks=picks3)
+dists4 = mne.preprocessing.nirs.source_detector_distances(raw4.info,picks=picks4)
+
+# %%
+
+# Short channels
+
+short_channels0 = raw0.copy().pick(picks0[dists0 <= 0.01])
+short_channels1 = raw1.copy().pick(picks1[dists1 <= 0.01])
+short_channels2 = raw2.copy().pick(picks2[dists2 <= 0.01])
+short_channels3 = raw3.copy().pick(picks3[dists3 <= 0.01])
+short_channels4 = raw4.copy().pick(picks4[dists4 <= 0.01])
+
+# Long channels
+
+long_channels0 = raw0.copy().pick(picks0[dists0 > 0.01])
+long_channels1 = raw1.copy().pick(picks1[dists1 > 0.01])
+long_channels2 = raw2.copy().pick(picks2[dists2 > 0.01])
+long_channels3 = raw3.copy().pick(picks3[dists3 > 0.01])
+long_channels4 = raw4.copy().pick(picks4[dists4 > 0.01])
+#%%
+
+# Visualize artifacts (only on the first subject) (mne overview of artifacts detection)
+raw = read_raw_bids(bids_paths[0])
+
+# Low-frequency drifts
+
+mag_channels0 = mne.pick_types(raw.info, meg="mag")
+raw.load_data()
+mne.viz.plot_raw(raw, duration=60, n_channels = len(raw.ch_names), scalings="auto")
+
+
+#%%
+def lastCompsPCA(x, n):
+    pca = PCA()
+    pca.fit(x)
+    #print(pca.explained_variance_ratio_.shape)
+    X_reduced = pca.transform(x)
+    #print(pca.explained_variance_ratio_)
+    return pca.inverse_transform(X_reduced)[:,n:]
+
+
+# # %%
+# # Motion Artifact Correction
+
+PCA_MAC = make_pipeline(PCA(n ))
+
+
