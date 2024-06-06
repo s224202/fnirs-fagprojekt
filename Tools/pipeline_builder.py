5e421e6e69ccd20c4bc2fca2c29e3c5c06f2c299
Adam <adam.bhh@live.com>
2024-06-06 10:45:16 +0200

    Began building pipelinebuilder

---
+++ Tools/pipeline_builder.py
@@ -0,0 +1,4 @@
+from sklearn.pipeline import Pipeline, make_pipeline
+def build_pipeline(motion:str, phys:str, systemic:str, classifier:str)-> Pipeline:
+    ''' some sort of docstring here '''
+    
