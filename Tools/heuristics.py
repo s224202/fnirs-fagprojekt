d24594378f1adf4bbbe13ef84c24a508eeb92c42
adam <s224202@dtu.dk>
2024-06-18 14:08:22 +0200

    More HPC_commits

--- Tools/heuristics.py
+++ Tools/heuristics.py
@@ -3,8 +3,10 @@ def compute_heuristics(data: np.ndarray) -> np.ndarray:
     heuristics = np.zeros((data.shape[0],data.shape[1],3))
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
-            peak_index, peak_value = (np.argmax(data[i,j]), np.max(data[i,j]))
-            peak_index = 1 if peak_index == 0 else peak_index
+            #peak_index, peak_value = (np.argmax(data[i,j]), np.max(data[i,j]))
+            #peak_index = 1 if peak_index == 0 else peak_index
+            peak_index = 79
+            peak_value = data[i,j][peak_index]
             mean = np.mean(data[i,j][:peak_index]) - np.mean(data[i,j][peak_index:])
             sd = np.std(data[i,j][:peak_index]) - np.std(data[i,j][peak_index:])
             slope = (peak_value - data[i,j][0])/(len(data[i,j])/2) - (peak_value - data[i,j][-1])/(len(data[i,j])/2)
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from re import I
 from Tools.pipeline_builder import build_pipeline
-from Tools.data_loaders import load_individual, load_author_data, concatenate_data
+from Tools.data_loaders import *
 from Tools.function_wrappers import feature_selection_wrapper
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, LeaveOneOut
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
 from sklearn.dummy import DummyClassifier
+from sklearn.metrics import confusion_matrix, make_scorer
 from sklearn.feature_selection import SequentialFeatureSelector
 import pandas as pd
+from sklearn.svm import SVC
 
 # Test the pipeline builder
 # control pipeline
@@ -42,13 +44,21 @@ regression_wiener = build_pipeline(systemic='Band pass', motion='Wiener', phys='
 regression_spline = build_pipeline(systemic='Band pass', motion='Spline', phys='Regression', classifier='None', split_epochs=True)
 
 # Test the feature selection wrapper
-model = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=10000, random_state=r)
+model = MLPClassifier(hidden_layer_sizes=(10), max_iter=10000, random_state=r, solver='adam', activation='relu')
+#model = SVC(random_state=r, kernel='linear')
 baselinemodel = DummyClassifier(strategy='most_frequent')
 datalist = [load_individual(0), load_individual(1), load_individual(2), load_individual(3), load_individual(4)]
 #datalist = [load_author_data(2), load_author_data(3), load_author_data(4)]
-labelslist = [datalist[i].annotations.to_data_frame()['description'] for i in range(len(datalist))]
+
 pipelines_list = [pipeline,bandpass, ica, bpca, regression, tddr, Wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline]
+#datalist = [load_CUH_data(1, 'Healthy'), load_CUH_data(2, 'Healthy'), load_CUH_data(3, 'Healthy'), load_CUH_data(4, 'Healthy'), 
+            #load_CUH_data(5, 'Healthy'), 
+            #load_CUH_data(5, 'Healthy'), load_CUH_data(7, 'Healthy')]
+#datalist = [load_CUH_data(1, 'DoC'), load_CUH_data(2, 'DoC'), load_CUH_data(5, 'DoC'), 
+#            #load_CUH_data(3, 'DoC'), load_CUH_data(6, 'DoC'),
+#            load_CUH_data(4, 'DoC'), load_CUH_data(7, 'DoC')]
 #data, labels = concatenate_data(datalist, labelslist)
+labelslist = [datalist[i].annotations.to_data_frame()['description'] for i in range(len(datalist))]
 label_encoder = LabelEncoder()
 for i in range(len(datalist)):
     results_list.append([])
@@ -56,20 +66,22 @@ for i in range(len(datalist)):
     labelslist[i] = label_encoder.fit_transform(labelslist[i])
     for j in range(len(pipelines_list)):
         newdata = pipelines_list[j].fit_transform(datalist[i])
-        sfs = SequentialFeatureSelector(model, n_features_to_select='auto', cv=3, tol=0.01)
-        sfs.fit(newdata, labelslist[i])
-        sfs.transform(newdata)
-        scores = cross_val_score(model, newdata, labelslist[i], cv=3, scoring='accuracy')
+        #sfs = SequentialFeatureSelector(model, n_features_to_select='auto', cv=3, tol=0.01)
+        #sfs.fit(newdata, labelslist[i])
+        #sfs.transform(newdata)
+        scores = cross_val_score(model, newdata, labelslist[i], cv=10, scoring='accuracy')
+        print(np.mean(scores))
         results_list[i].append((scores.mean(), scores.std()))
-        scores2 = cross_val_score(baselinemodel,newdata, labelslist[i], cv=3)
-        baseline_results[i].append((scores2.mean(), scores2.std()))
-        print(f'{(i*17+j)/(len(datalist)*17)*100}% done')
-        open('results.txt', 'a').write(sfs.get_support().__str__() + '\n' + scores.__str__() + '\n' + scores2.__str__() + '\n')
+#         #scores2 = cross_val_score(baselinemodel,newdata, labelslist[i], cv=3)
+        #baseline_results[i].append((scores2.mean(), scores2.std()))
+        #print(f'{(i*17+j)/(len(datalist)*17)*100}% done')
+        #open('results.txt', 'a').write(sfs.get_support().__str__() + '\n' + scores.__str__() + '\n' + scores2.__str__() + '\n')
 
 df = pd.DataFrame(results_list)
+print(df)
 df.T.to_csv('results.csv')
-df = pd.DataFrame(baseline_results)
-df.T.to_csv('baseline_results.csv')
+# df = pd.DataFrame(baseline_results)
+# df.T.to_csv('baseline_results.csv')
 
 
 #individual 0: (78, 120) labels = (90,)
@@ -77,3 +89,17 @@ df.T.to_csv('baseline_results.csv')
 #individual 2: (28, 96)  labels = (90,)
 #individual 3: (35, 156) labels = (90,)
 #individual 4: (61, 78)  labels = (90,)
+
+# test_pipeline1 = build_pipeline(systemic='None', motion='None', phys='None', classifier='None', split_epochs=False)
+# test_pipeline2 = build_pipeline(systemic='Band pass', motion='None', phys='None', classifier='None', split_epochs=False)
+
+# data = load_individual(0)
+# data2 = load_individual(0)
+
+# newdata = test_pipeline1.fit_transform(data)
+# newdata2 = test_pipeline2.fit_transform(data2)
+
+# newdata.plot(n_channels=len(newdata.ch_names), title='No Correction', duration = 500)
+# newdata2.plot(n_channels=len(newdata2.ch_names), title='Bandpass + TDDR + bPCA', duration = 500)
+# plt.show()
+
