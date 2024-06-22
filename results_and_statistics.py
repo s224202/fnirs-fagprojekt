0400512653242f4a510c3492d3c1da56fb595fcb
Signe Molsted <s221730@student.dtu.dk>
2024-06-22 10:31:37 +0200

    statistics on cuh data

--- results_and_statistics.py
+++ results_and_statistics.py
@@ -2,21 +2,155 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
-from scikit_posthocs import posthoc_dunn
 
+# For Riget
 # read csv files
-df = pd.read_csv('Results/results.csv', sep=',', header=None)
-
-dfbase = pd.read_csv('Results/baseline_results.csv', sep=',', header=None)
+df = pd.read_csv('Results/resultsriget.csv', sep=',', header=None)
+# print(df.iloc[0, 1::2])
 
 #%%
 # calculate mean and sd of all pipelines
 
 for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
-    print(f'Mean of pipeline {pipeline}: {round(np.mean(df.iloc[row, 1::2]), 2)}', f'SD of pipeline {pipeline}: {round(np.mean(df.iloc[row, 2::2]),2)}')
+    print(f'Mean of pipeline {pipeline}: {round(np.mean(df.iloc[row, 1::6]), 3)}', f'SD of pipeline {pipeline}: {round(np.mean(df.iloc[row, 2::6]), 3)}')
 
-# calculate mean and sd of baseline
-print(f'Mean of baseline: {round(np.mean(dfbase.iloc[0, 1::2]), 2)}', f'SD of baseline: {round(np.mean(dfbase.iloc[0, 2::2]),2)}')
+# calculate mean and sd of recall of all pipelines
+for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
+    print(f'Mean of recall {pipeline}: {round(np.mean(df.iloc[row, 3::6]), 3)}', f'SD of recall {pipeline}: {round(np.mean(df.iloc[row, 4::6]), 3)}')
+
+# calculate f1-score of all pipelines
+for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
+    print(f'Mean of f1-score {pipeline}: {round(np.mean(df.iloc[row, 5::6]), 3)}', f'SD of f1-score {pipeline}: {round(np.mean(df.iloc[row, 6::6]), 3)}')
+
+#%%
+none = df.iloc[0, 1::6]
+bandpass = df.iloc[1, 1::6]
+ica = df.iloc[2, 1::6]
+bpca = df.iloc[3, 1::6]
+regression = df.iloc[4, 1::6]
+tddr = df.iloc[5, 1::6]
+wiener = df.iloc[6, 1::6]
+spline = df.iloc[7, 1::6]
+ica_tddr = df.iloc[8, 1::6]
+ica_wiener = df.iloc[9, 1::6]
+ica_spline = df.iloc[10, 1::6]
+bpca_tddr = df.iloc[11, 1::6]
+bpca_wiener = df.iloc[12, 1::6]
+bpca_spline = df.iloc[13, 1::6]
+print(bpca_spline)
+regression_tddr = df.iloc[14, 1::6]
+regression_wiener = df.iloc[15, 1::6]
+regression_spline = df.iloc[16, 1::6]
+baseline = 0.5
+#%%
+# are the variances equal?
+print(f'Are the pipelines variances equal?', stats.bartlett(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue > 0.05, stats.bartlett(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue)
+
+#%%
+# shapiro-wilks
+for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
+    print(f'Is {pipeline} normally distributed?', stats.shapiro(df.iloc[row, 1::6]).pvalue > 0.05)
+
+# print(f'Is baseline normally distributed?', stats.shapiro(dfbase.iloc[0, 1::2]).pvalue > 0.05)
+
+#%%
+# calculate one-way ANOVA and Kruskal-Wallis (without baseline)
+
+print(f'Is there a statistical significant difference between the means?', stats.f_oneway(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue < 0.05, stats.f_oneway(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue)
+print(f'Is there a statistical significant difference between the means? (no distribution assumptions)', stats.kruskal(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue < 0.05, stats.kruskal(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue)
+
+#%%
+# mann whitney U test for pipelines vs baseline
+for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
+    print(f'Mann-Whitney test for {pipeline} and baseline:', stats.mannwhitneyu(df.iloc[row, 1::6], [0.5 for _ in range(len(df.iloc[row, 1::6]))], nan_policy='omit').pvalue, stats.mannwhitneyu(df.iloc[row, 1::6], [0.5 for _ in range(len(df.iloc[row,1::6]))], nan_policy='omit').pvalue < 0.05)
+
+#%%
+
+print(df.shape)
+Patient1 = df.iloc[:, 1]
+Patient2 = df.iloc[:, 7]
+Patient3 = df.iloc[:, 13]
+Patient4 = df.iloc[:, 19]
+Patient5 = df.iloc[:, 25]
+Patient6 = df.iloc[:, 31]
+Patient7 = df.iloc[:, 37]
+Patient8 = df.iloc[:, 43]
+Patient9 = df.iloc[:, 49]
+Patient10 = df.iloc[:, 55]
+Patient11 = df.iloc[:, 61]
+Patient12 = df.iloc[:, 67]
+Patient13 = df.iloc[:, 73]
+Patient14 = df.iloc[:, 79]
+Patient15 = df.iloc[:, 85]
+Patient16 = df.iloc[:, 91]
+Patient17 = df.iloc[:, 97]
+Patient18 = df.iloc[:, 103]
+Patient19 = df.iloc[:, 109]
+Patient20 = df.iloc[:, 115]
+Patient21 = df.iloc[:, 121]
+Patient22 = df.iloc[:, 127]
+Patient23 = df.iloc[:, 133]
+Patient24 = df.iloc[:, 139]
+Patient25 = df.iloc[:, 145]
+Patient26 = df.iloc[:, 151]
+Patient27 = df.iloc[:, 157]
+Patient28 = df.iloc[:, 163]
+Patient29 = df.iloc[:, 169]
+Patient30 = df.iloc[:, 175]
+Patient31 = df.iloc[:, 181]
+Patient32 = df.iloc[:, 187]
+Patient33 = df.iloc[:, 193]
+Patient34 = df.iloc[:, 199]
+Patient35 = df.iloc[:, 205]
+Patient36 = df.iloc[:, 211]
+
+print(Patient2)
+
+#%%
+
+for column, patient in enumerate(["Column", "Patient 1", "Patient 2", "Patient 3","Patient 4","Patient 5","Patient 6","Patient 7","Patient 8","Patient 9","Patient 10","Patient 11","Patient 12","Patient 13","Patient 14","Patient 15","Patient 16","Patient 17","Patient 18","Patient 19","Patient 20","Patient 21","Patient 22","Patient 23","Patient 24","Patient 25","Patient 26","Patient 27","Patient 28","Patient 29","Patient 30","Patient 31","Patient 32","Patient 33","Patient 34","Patient 35","Patient 36"]):
+    print(f'Mean of {patient}: {round(np.mean(df.iloc[:, 1::6]), 3)}', f'SD of {patient}: {round(np.mean(df.iloc[:, 2::6]), 3)}')
+#%%
+
+# are the variances equal?
+print(f'Are the pipelines variances equal?', stats.bartlett(Patient1, Patient2, Patient3, Patient4, Patient5, Patient6, Patient7, Patient8, Patient9, Patient10, Patient11, Patient12, Patient13, Patient14, Patient15, Patient16, Patient17, Patient18, Patient19, Patient20, Patient21, Patient22, Patient23, Patient24, Patient25, Patient26, Patient27, Patient28, Patient29, Patient30, Patient31, Patient32, Patient33, Patient34, Patient35, Patient36, nan_policy='omit').pvalue > 0.05, stats.bartlett(Patient1, Patient2, Patient3, Patient4, Patient5, Patient6, Patient7, Patient8, Patient9, Patient10, Patient11, Patient12, Patient13, Patient14, Patient15, Patient16, Patient17, Patient18, Patient19, Patient20, Patient21, Patient22, Patient23, Patient24, Patient25, Patient26, Patient27, Patient28, Patient29, Patient30, Patient31, Patient32, Patient33, Patient34, Patient35, Patient36, nan_policy='omit').pvalue)
+
+#%%
+
+# shapiro-wilks
+for column, patient in enumerate(["Column", "Patient 1", "Patient 2", "Patient 3","Patient 4","Patient 5","Patient 6","Patient 7","Patient 8","Patient 9","Patient 10","Patient 11","Patient 12","Patient 13","Patient 14","Patient 15","Patient 16","Patient 17","Patient 18","Patient 19","Patient 20","Patient 21","Patient 22","Patient 23","Patient 24","Patient 25","Patient 26","Patient 27","Patient 28","Patient 29","Patient 30","Patient 31","Patient 32","Patient 33","Patient 34","Patient 35","Patient 36"]):
+    print(f'Is {patient} normally distributed?', stats.shapiro(df.iloc[:, 1::6]).pvalue > 0.05)
+#%%
+#PROBLEM
+# mann-whitney u for patients vs baseline
+for column, patient in enumerate(["Column", "Patient 1", "Patient 2", "Patient 3","Patient 4","Patient 5","Patient 6","Patient 7","Patient 8","Patient 9","Patient 10","Patient 11","Patient 12","Patient 13","Patient 14","Patient 15","Patient 16","Patient 17","Patient 18","Patient 19","Patient 20","Patient 21","Patient 22","Patient 23","Patient 24","Patient 25","Patient 26","Patient 27","Patient 28","Patient 29","Patient 30","Patient 31","Patient 32","Patient 33","Patient 34","Patient 35","Patient 36"]):
+    print(f't-test for {patient} and baseline:', (stats.mannwhitneyu(df.iloc[:, 1:126:6], [0.5 for _ in range(len(df.iloc[:, 1:126:6]))], nan_policy='omit').pvalue, stats.mannwhitneyu(df.iloc[:, 1:126:6], [0.5 for _ in range(len(df.iloc[:, 1:126:6]))], nan_policy='omit').pvalue < 0.05))
+#%%
+print(df.iloc[:, 1:126:6])
+#%%
+
+for column, patient in enumerate(["Column", "Patient 1", "Patient 2", "Patient 3","Patient 4","Patient 5","Patient 6","Patient 7","Patient 8","Patient 9","Patient 10","Patient 11","Patient 12","Patient 13","Patient 14","Patient 15","Patient 16","Patient 17","Patient 18","Patient 19","Patient 20","Patient 21","Patient 22","Patient 23","Patient 24","Patient 25","Patient 26","Patient 27","Patient 28","Patient 29","Patient 30","Patient 31","Patient 32","Patient 33","Patient 34","Patient 35","Patient 36"]):
+    print(f't-test for {patient} and baseline:', (stats.ttest_ind(df.iloc[:, 1::6], [0.5 for _ in range(len(df.iloc[:, 1::6]))], nan_policy='omit').pvalue, stats.ttest_ind(df.iloc[:, 1::6], [0.5 for _ in range(len(df.iloc[:, 1::6]))], nan_policy='omit').pvalue < 0.05))
+
+
+
+
+
+
+
+
+#%%
+# For Rob Lukes
+# read csv files
+df = pd.read_csv('Results/results.csv', sep=',', header=None)
+# print(df.iloc[0, 1::2])
+
+#%%
+# calculate mean and sd of all pipelines
+
+for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
+    print(f'Mean of pipeline {pipeline}: {round(np.mean(df.iloc[row, 1::2]), 3)}', f'SD of pipeline {pipeline}: {round(np.mean(df.iloc[row, 2::2]), 3)}')
+
 
 #%%
 none = df.iloc[0, 1::2]
@@ -36,26 +170,23 @@ bpca_spline = df.iloc[13, 1::2]
 regression_tddr = df.iloc[14, 1::2]
 regression_wiener = df.iloc[15, 1::2]
 regression_spline = df.iloc[16, 1::2]
-baseline = dfbase.iloc[0, 1::2]
+baseline = 0.3333
+#%%
 # are the variances equal?
-print(stats.bartlett(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline))
+print(f'Are the pipelines variances equal?', stats.bartlett(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue > 0.05, stats.bartlett(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue)
 
 #%%
 # shapiro-wilks
 for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
     print(f'Is {pipeline} normally distributed?', stats.shapiro(df.iloc[row, 1::2]).pvalue > 0.05)
 
-print(f'Is baseline normally distributed?', stats.shapiro(dfbase.iloc[0, 1::2]).pvalue > 0.05)
-
 #%%
 # calculate one-way ANOVA and Kruskal-Wallis (without baseline)
 
-print(f'Is there a statistical significant difference between the means?', stats.f_oneway(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline).pvalue < 0.05)
-print(f'Is there a statistical significant difference between the means? (no distribution assumptions)', stats.kruskal(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline).pvalue < 0.05)
+print(f'Is there a statistical significant difference between the means?', stats.f_oneway(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue < 0.05, stats.f_oneway(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue)
+print(f'Is there a statistical significant difference between the means? (no distribution assumptions)', stats.kruskal(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue < 0.05, stats.kruskal(none, bandpass, ica, bpca, regression, tddr, wiener, spline, ica_tddr, ica_wiener, ica_spline, bpca_tddr, bpca_wiener, bpca_spline, regression_tddr, regression_wiener, regression_spline, nan_policy='omit').pvalue)
 
 #%%
-# paired t-test for baseline
+# t-test for baseline
 for row, pipeline in enumerate(["None", "Band-pass", "ICA", "bPCA", "sc regression", "TDDR", "Wiener", "Spline", "ICA-TDDR", "ICA-Wiener", "ICA-Spline", "bPCA-TDDR", "bPCA-Wiener", "bPCA-Spline", "sc regression-TDDR", "sc regression-Wiener", "sc regression-Spline"]):
-    print(f'Paired t-test for {pipeline} and baseline:', stats.ttest_rel(df.iloc[row, 1::2], baseline).pvalue, stats.ttest_rel(df.iloc[row, 1::2], baseline).pvalue < 0.05)
-
-# %%
+    print(f't-test for {pipeline} and baseline:', stats.false_discovery_control(stats.ttest_ind(df.iloc[row, 1::2], [0.5 for _ in range(len(df.iloc[row, 1::2]))], nan_policy='omit').pvalue, method='bh'), stats.ttest_ind(df.iloc[row, 1::2], [0.5 for _ in range(len(df.iloc[row,1::2]))], nan_policy='omit').pvalue < 0.05)
